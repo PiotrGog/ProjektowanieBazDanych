107e7b61ef674001515a298b9758a58c0ab55de5
cos <kropaczmarcin@gmail.com>
2019-01-14 20:15:18 +0100

    Modifers and buffs

--- database/src/main/java/pbd/entity/Buff.java
+++ database/src/main/java/pbd/entity/Buff.java
@@ -7,12 +7,43 @@ public class Buff extends Modifier {
 
     private int duration;
 
-    public Buff(String modName, String description, int modValue, int duration) {
+    protected Buff(String modName, String description, int modValue, int duration) {
         super(modName, description, modValue);
         this.duration = duration;
     }
 
     public int getDuration() {
         return duration;
+    }
+
+    public void setDuration(int duration) throws IllegalArgumentException, NullPointerException {
+        if (duration < 1) {
+            throw new IllegalArgumentException("Duration value can not be less than 1.");
+        }
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return  "Buff: " +
+                "buff name=" + modName + 
+                ", buff value=" + modValue + 
+                ", buff time=" + duration + "s"
+                "\n" + Description;
+    }
+
+    public static Class classFactory(String modName, String description, int modValue, int duration)
+    throw IllegalArgumentException
+    {
+    if (description == "") {
+            throw new IllegalArgumentException("Description value can not be an empty string.");
+        }
+    if (modName == "") {
+            throw new IllegalArgumentException("Modifer Name value can not be an empty string.");
+        }
+    if (duration< 1) {
+            throw new IllegalArgumentException("Duration value can not be less than 1.");
+        }
+    return new Buff(modName,description, modValue, duration);        
     }
 }
--- database/src/main/java/pbd/entity/Modifier.java
+++ database/src/main/java/pbd/entity/Modifier.java
@@ -15,7 +15,7 @@ public class Modifier {
 
     private int modValue;
 
-    public Modifier(String modName, String description, int modValue) {
+   protected Modifier(String modName, String description, int modValue) {
         this.modName = modName;
         this.description = description;
         this.modValue = modValue;
@@ -31,5 +31,44 @@ public class Modifier {
 
     public int getModValue() {
         return modValue;
+    }
+    public void setModValue(int modValue){
+        this.modValue = modValue;
+    }
+
+    public void setDescription(String description) throws IllegalArgumentException {
+        if (description == "") {
+            throw new IllegalArgumentException("Description value can not be an empty string.");
+        }
+        this.description=description;
+    }
+    
+    public void setModeName(String modName) throws IllegalArgumentException {
+        if (modName == "") {
+            throw new IllegalArgumentException("Modifer Name value can not be an empty string.");
+        }
+        this.modName=modName;
+    }
+
+    @Override
+    public String toString() {
+        return "Modifer: " +
+                "modifer name=" + modName + 
+                ", modifer value=" + modValue + 
+                "\n" + Description;
+    }
+
+
+    public static Class classFactory(String modName, String description, int modValue)
+    throw IllegalArgumentException
+    {
+    if (description == "") {
+            throw new IllegalArgumentException("Description value can not be an empty string.");
+        }
+    if (modName == "") {
+            throw new IllegalArgumentException("Modifer Name value can not be an empty string.");
+        }
+    
+    return new Modifer(modName, description, modValue)
     }
 }
